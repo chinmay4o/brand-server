f15b942e825b399f2523efce6bec5d8f18f39ffa
surve4404 <surve4404@gmail.com>
2021-12-26 10:46:51 +0530

    added recharge route

--- router/customer.js
+++ router/customer.js
@@ -1,5 +1,6 @@
 import express from "express";
 import { Customers } from "../models/customerSchema.js";
+import { Recharge } from "../models/rechargeSchema.js";
 
 const router = express.Router();
 
@@ -14,7 +15,7 @@ router.post("/data", async (req, res) => {
   const { cname, email, number, address } = req.body;
 
   const newData = new Customers({
-    name : cname,
+    name: cname,
     email,
     number,
     address,
@@ -23,6 +24,26 @@ router.post("/data", async (req, res) => {
   await newData.save();
 
   res.json(newData);
+});
+
+//   adding recharge customers to the database
+router.post("/recharge", async (req, res) => {
+  const { cname, number } = req.body;
+
+  const newData = new Recharge({
+    name: cname,
+    number,
+  });
+
+  await newData.save();
+
+  res.json(newData);
+});
+
+//   sending recharge customers to the database
+router.get("/recharge", async (req, res) => {
+  const data = await Recharge.find();
+  res.json(data);
 });
 
 export const customerRouter = router;
