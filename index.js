8d8233c9cf0992372a57f4575616ba4050509ef0
surve4404 <surve4404@gmail.com>
2021-12-28 16:26:20 +0530

    cors

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ import express from 'express';
 import cors from 'cors';
 import dotenv from "dotenv";
 import mongoose from "mongoose";
+import bodyParser from "body-parser";
 import {customerRouter} from "./router/customer.js";
 
 
@@ -13,12 +14,20 @@ app.use(
   })
 );
 
-var dd = {
-    origin : "*",
-    credentials : true
-  };
+app.use(cors({
+  origin: true
+  }));
 
-app.use(cors(dd));
+// parse requests of content-type - application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: true }))
+
+app.use(function (req, res, next) {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Headers', 'Origins, X-Requested-With, Content-Type, Accept');
+    res.header("Access-Control-Allow-Method", "*");
+    next();
+})
+
 
 dotenv.config({ path : "./config.env"});
 
