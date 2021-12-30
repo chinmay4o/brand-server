09c8a3898f6951cd74abe1636b70eea2a9623536
surve4404 <surve4404@gmail.com>
2021-12-30 11:55:55 +0530

    cors

--- index.js
+++ index.js
@@ -13,6 +13,14 @@ app.use(cors({
   credentials: true
   }));
 
+  app.use(function (req, res, next) {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Headers', 'Origins, X-Requested-With, Content-Type, Accept');
+    res.header("Access-Control-Allow-Method", "*");
+    next();
+})
+
+
 
 app.use(
   express.json({
