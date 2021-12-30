7bffc0aa345cf3bc6232363801f3826e503911d9
surve4404 <surve4404@gmail.com>
2021-12-30 13:51:31 +0530

    cors

--- index.js
+++ index.js
@@ -1,44 +1,33 @@
-import express from 'express';
-import cors from 'cors';
+import express from "express";
+import cors from "cors";
 import dotenv from "dotenv";
 import mongoose from "mongoose";
-import bodyParser from "body-parser";
-import {customerRouter} from "./router/customer.js";
-dotenv.config({ path : "./config.env"});
+import { customerRouter } from "./router/customer.js";
+dotenv.config({ path: "./config.env" });
 
-const app  = express();
-
-app.use(cors({
-  origin: "https://onebrand4o.netlify.app/",
-  credentials: true
-  }));
-
-  app.use(function (req, res, next) {
-    res.header('Access-Control-Allow-Origin', '*');
-    res.header('Access-Control-Allow-Headers', 'Origins, X-Requested-With, Content-Type, Accept');
-    res.header("Access-Control-Allow-Method", "*");
-    next();
-})
-
-
+const app = express();
 
 app.use(
-  express.json({
-    extended: false,
+  cors({
+    origin: "https://onebrand4o.netlify.app/",
+    credentials: true,
   })
 );
 
+app.use(express.json());
 
 //mongodb connection
 const url = process.env.DATABASE;
 
-mongoose.connect(url, { 
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
+mongoose.connect(url, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
 });
 const onn = mongoose.connection;
-onn.on("open" , () => console.log("mongodb connected"));
+onn.on("open", () => console.log("mongodb connected"));
 
 app.use("/", customerRouter);
 
-app.listen(process.env.PORT , () => console.log("listening on port " + process.env.PORT));
+app.listen(process.env.PORT, () =>
+  console.log("listening on port " + process.env.PORT)
+);
