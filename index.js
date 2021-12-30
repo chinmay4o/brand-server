839b6144c90b116d29b8f495aae9592a2477cbd5
surve4404 <surve4404@gmail.com>
2021-12-30 15:01:37 +0530

    Initial commit

--- index.js
+++ index.js
@@ -18,6 +18,11 @@ mongoose.connect(url , {useNewUrlParser : true , useUnifiedTopology : true});
 const conn = mongoose.connection;
 conn.on("open" , () => console.log("mongodb connected"));
 
+app.get("/" , (req , res) => {
+  res.send("api in running...")
+});
+
+
 app.use("/", customerRouter);
 
 app.listen(process.env.PORT , () => console.log("listening on " + process.env.PORT));
